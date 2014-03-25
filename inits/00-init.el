69b8f14dbf98ee837ad8dac11bff58a0d79dcfc5
Hayato Kajiyama <hayato@cheddar.local>
2014-03-25 19:17:55 +0900

    emacsの文字コード修正

--- inits/00-init.el
+++ inits/00-init.el
@@ -11,18 +11,6 @@
 ;;;;;;;;;;emacs本体の設定;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;文字コード設定
-(setq default-buffer-file-coding-system 'utf-8-unix)
-(set-buffer-file-coding-system 'utf-8-unix)
-(set-terminal-coding-system 'utf-8-unix)
-(set-keyboard-coding-system 'utf-8-unix)
-(set-clipboard-coding-system 'utf-8-unix)
-(prefer-coding-system 'utf-8-unix)
-(set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8-unix)
-(set-language-environment "Japanese")
-(setq locale-coding-system 'utf-8)
-
 ;; scratchの初期メッセージ消去
 (setq initial-scratch-message "")
 
