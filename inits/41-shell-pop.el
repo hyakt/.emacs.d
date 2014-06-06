c3fc3d3c25117ffd4bd4bed4b1f1e6f0a4a80f22
Hayato Kajiyama <me@piginbeer.com>
2014-06-07 04:07:01 +0900

    emacsのshell-popの設定をコメントアウト

--- inits/41-shell-pop.el
+++ inits/41-shell-pop.el
@@ -1,13 +1,13 @@
-(require 'shell-pop)
+;; (require 'shell-pop)
 
-(custom-set-variables
- '(shell-pop-set-internal-mode "multi-term")
- '(shell-pop-set-internal-mode-shell "/bin/bash")
- '(shell-pop-set-window-height 45)
- '(shell-pop-set-window-position "bottom")
- '(shell-pop-shell-type (quote ("multi-term" "*pop-terminal*" (lambda nil (multi-term)))))
- '(shell-pop-term-shell "/bin/zsh")
-; '(shell-pop-universal-key "C-t")
- '(shell-pop-window-height 30))
+;; (custom-set-variables
+;;  '(shell-pop-set-internal-mode "multi-term")
+;;  '(shell-pop-set-internal-mode-shell "/bin/bash")
+;;  '(shell-pop-set-window-height 45)
+;;  '(shell-pop-set-window-position "bottom")
+;;  '(shell-pop-shell-type (quote ("multi-term" "*pop-terminal*" (lambda nil (multi-term)))))
+;;  '(shell-pop-term-shell "/bin/zsh")
+;; ; '(shell-pop-universal-key "C-t")
+;;  '(shell-pop-window-height 30))
 
-(global-set-key (kbd "C-t") 'shell-pop)
+;; (global-set-key (kbd "C-t") 'shell-pop)
