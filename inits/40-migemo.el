541caa2f322afe3f5b664b5dfefc85994e37e80e
Hayato Kajiyama <me@piginbeer.com>
2014-06-10 00:21:57 +0900

    emacsのmigemoの修正 .gitignoreでelpaのarchivesを無視

--- inits/40-migemo.el
+++ inits/40-migemo.el
@@ -1,7 +1,9 @@
 ;;
 ;; C/Migemo
 ;;
-(when (and (executable-find "cmigemo")
+
+(when (eq system-type 'darwin)          ;mac
+  (when (and (executable-find "cmigemo")
            (require 'migemo nil t))
   (setq migemo-command "cmigemo")
   (setq migemo-options '("-q" "--emacs" "-i" "\g"))
@@ -16,4 +18,21 @@
 
   (load-library "migemo")
   (migemo-init)
-)
+  )
+  )
+
+(when (eq system-type 'windows-nt)      ;windows
+  (setq migemo-command (concat (getenv "INST_DIR")
+                               "~\\cmigemo\\cmigemo"))
+  (setq migemo-options '("-q" "--emacs"))
+  (setq migemo-dictionary (concat (getenv "INST_DIR")
+                                  "~\\cmigemo\\dict\\utf-8\\migemo-dict"))
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-use-pattern-alist t)
+  (setq migemo-use-frequent-pattern-alist t)
+  (setq migemo-pattern-alist-length 1024)
+  (setq migemo-coding-system 'utf-8-unix)
+  (load-library "migemo")
+  )
+
