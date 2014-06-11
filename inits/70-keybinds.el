ed1dd754c1ef8896bfa78b42a9ee95fa90806434
Hayato Kajiyama <me@piginbeer.com>
2014-06-12 00:59:46 +0900

    emacsのkeybindの設定

---
+++ inits/70-keybinds.el
@@ -0,0 +1,36 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;キーバインドの設定;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'key-chord)
+(key-chord-mode 1)
+(setq key-chord-two-keys-delay 0.05)
+
+(require 'key-combo)
+(key-combo-mode 0)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;; 通常操作 ;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key "\C-h" 'delete-backward-char)
+(global-set-key "\C-x\C-i" 'indent-region) ; 選択範囲をインデント
+(global-set-key "\C-m" 'newline-and-indent) ; リターンで改行とインデント
+(global-set-key "\C-j" 'newline) ; 改行
+(global-set-key (kbd "C-c a")   'align)
+(global-set-key (kbd "C-c M-a") 'align-regexp)
+(define-key global-map (kbd "C-c C-a") 'delete-trailing-whitespace)
+
+;; 複数行移動
+(global-set-key "\M-n" (kbd "C-u 5 C-n"))
+(global-set-key "\M-p" (kbd "C-u 5 C-p"))
+
+(global-set-key (kbd "C-x ?") 'help-command)
+
+;;; 少しずつスクロール
+(define-key global-map (kbd "C-s-n") 'scroll-down-in-place)
+(define-key global-map (kbd "C-s-p") 'scroll-up-in-place)
+
+;; redo
+(global-set-key "\M-/" 'redo)
+
+;;; quickrun
+(global-set-key "\C-cc" 'quickrun-with-arg)
