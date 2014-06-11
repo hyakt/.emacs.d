60a2fa5cd90871f8a98fe57ea2537fd8abf8bdf4
Hayato Kajiyama <me@piginbeer.com>
2014-06-12 00:31:31 +0900

    emacsのinit-load時にエラーがはかれていたところの修正

--- inits/40-smartrep.el
+++
@@ -1,6 +0,0 @@
-(require 'smartrep)
-
-(smartrep-define-key
- global-map "C-." '(("C-n" . 'mc/mark-next-like-this)
-                    ("C-p" . 'mc/mark-previous-like-this)
-                    ("*"   . 'mc/mark-all-like-this)))
