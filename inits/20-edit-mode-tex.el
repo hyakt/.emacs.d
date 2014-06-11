75e0170841f7ea5d52a052c4ba181cc2f29b5027
Hayato Kajiyama <me@piginbeer.com>
2014-06-11 22:53:50 +0900

    emacs用のtex設定を追加

---
+++ inits/20-edit-mode-tex.el
@@ -0,0 +1,21 @@
+;; latexの設定(auctex)
+(require 'tex-site)
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+;; auctex用のlatexmkを追加
+(add-hook 'LaTeX-mode-hook
+          (function (lambda ()
+                      (require 'auctex-latexmk nil 'noerror)
+                      (auctex-latexmk-setup)
+                      )))
+
+;; auctex用のauto-completeを読み込み
+(eval-after-load 'latex
+ '(require 'auto-complete-auctex))
+
+;; reftexの設定
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
