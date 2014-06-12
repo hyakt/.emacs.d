ee5ccbd6be50f114bf01f6f177b2bef0cd1b5cff
Hayato Kajiyama <me@piginbeer.com>
2014-06-12 23:38:32 +0900

    emacsのhelm関係の編集

---
+++ inits/31-helm-for-document.el
@@ -0,0 +1,30 @@
+(require 'helm-man)
+(require 'helm-elisp)
+
+(setq helm-for-document-sources
+      (list
+       helm-source-man-pages))
+
+(setq helm-for-document-sources
+      (nconc
+       (mapcar (lambda (func)
+                 (funcall func))
+               '(helm-def-source--emacs-commands
+                 helm-def-source--emacs-functions
+                 helm-def-source--emacs-variables
+                 helm-def-source--emacs-faces
+                 helm-def-source--helm-attributes))
+       '(helm-source-man-pages)))
+
+(defun helm-for-document ()
+  "Preconfigured `helm' for helm-for-document."
+  (interactive)
+  (helm
+   :sources helm-for-document-sources
+   :input (thing-at-point 'symbol)
+   :resume nil
+   :preselect nil
+   :buffer "*helm for document*"
+   :candidate-number-limit 20
+   )
+  )
