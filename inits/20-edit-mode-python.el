56bbd62e2c9d81089aab62a3d4abb5526391db1d
Hayato Kajiyama <me@piginbeer.com>
2014-06-15 22:30:25 +0900

    emacsのpythonとtexモードの修正

--- inits/20-edit-mode-python.el
+++ inits/20-edit-mode-python.el
@@ -1,26 +1,22 @@
 (require 'python)
-(require 'epc)
-(require 'jedi)
 (require 'py-autopep8)
 
-;; タブ幅の設定
-(add-hook 'python-mode-hook '(lambda () 
-                               (setq-default indent-tabs-mode nil)
-                               (setq-default tab-width 4)
-                               (define-key python-mode-map "\"" 'electric-pair)
-                               (define-key python-mode-map "\'" 'electric-pair)
-                               (define-key python-mode-map "(" 'electric-pair)
-                               (define-key python-mode-map "[" 'electric-pair)
-                               (define-key python-mode-map "{" 'electric-pair)))
-(defun electric-pair ()
-  "Insert character pair without sournding spaces"
-  (interactive)
-  (let (parens-require-spaces)
-    (insert-pair)))
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq tab-width 4)
+            (add-hook 'before-save-hook 'py-autopep8-before-save)
 
-;; jediの設定
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
+            ;; Macならjediを読み込む
+            (when (eq system-type 'darwin)
+              (require 'jedi)
+              (add-hook 'python-mode-hook
+                        (jedi:setup)
+                        (setq jedi:complete-on-dot t)
+                        )
+              )
+            )
+          )
 
 
-(add-hook 'before-save-hook 'py-autopep8-before-save)
+
--- inits/20-edit-mode-tex.el
+++ inits/20-edit-mode-tex.el
@@ -1,21 +1,23 @@
 ;; latexの設定(auctex)
-(require 'tex-site)
+(when (require 'tex-site nil t)
+  (require 'tex-site)
 
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq-default TeX-master nil)
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq-default TeX-master nil)
 
-;; auctex用のlatexmkを追加
-(add-hook 'LaTeX-mode-hook
-          (function (lambda ()
-                      (require 'auctex-latexmk nil 'noerror)
-                      (auctex-latexmk-setup)
-                      )))
+  ;; auctex用のlatexmkを追加
+  (add-hook 'LaTeX-mode-hook
+            (function (lambda ()
+                        (require 'auctex-latexmk nil 'noerror)
+                        (auctex-latexmk-setup)
+                        )))
 
-;; auctex用のauto-completeを読み込み
-(eval-after-load 'latex
- '(require 'auto-complete-auctex))
+  ;; auctex用のauto-completeを読み込み
+  (eval-after-load 'latex
+    '(require 'auto-complete-auctex))
 
-;; reftexの設定
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
+  ;; reftexの設定
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (setq reftex-plug-into-AUCTeX t)
+  )
