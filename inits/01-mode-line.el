983d626b8e3b8dad66be30e7d3d3d065e87b743f
Hayato Kajiyama <me@piginbeer.com>
2014-08-09 02:05:03 +0900

    emacsのpowerlineの見た目を変更

--- inits/01-mode-line.el
+++ inits/01-mode-line.el
@@ -1,3 +1,39 @@
 ;; milkypostman/powerline · GitHub
 (require 'powerline)
+
+(set-face-attribute 'powerline-active1 nil
+                    :foreground "#fff"
+                    :background "#89BA27"
+                    :inherit 'mode-line)
+
+(set-face-attribute 'powerline-active2 nil
+                    :foreground "#000"
+                    :background "#C2EB6F"
+                    :inherit 'mode-line)
+
 (powerline-default-theme)
+
+(defvar mode-line-cleaner-alist
+  '( ;; For minor-mode, first char is 'space'
+    (undo-tree-mode . "")
+    (git-gutter-mode . "")
+    (anzu-mode . "")
+    (yas-minor-mode . "")
+    (guide-key-mode . "")
+    (auto-complete-mode . "")
+    (global-whitespace-mode . "")
+    (magit-auto-revert-mode . "")
+    (smartparens-mode . "")
+    (helm-mode . "")
+    ))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for (mode . mode-str) in mode-line-cleaner-alist
+        do
+        (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
+          (when old-mode-str
+            (setcar old-mode-str mode-str))
+          )))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
