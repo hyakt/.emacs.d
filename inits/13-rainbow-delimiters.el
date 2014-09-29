947165f0ea311a48412870f7e23049fa3eaa9d20
Hayato Kajiyama <kaji1216@gmail.com>
2014-09-29 17:37:30 +0900

    emacsのrainbow-delimitersの設定を変更

--- inits/13-rainbow-delimiters.el
+++ inits/13-rainbow-delimiters.el
@@ -1,3 +1,4 @@
 (require 'rainbow-delimiters)
-(global-rainbow-delimiters-mode t)
+(add-to-list 'rainbow-delimiters-ignore-modes 'fundamental-mode)    
 (custom-set-faces '(rainbow-delimiters-depth-2-face ((t (:foreground "#E0FFB0")))))
+(global-rainbow-delimiters-mode 1)
