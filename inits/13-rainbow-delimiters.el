cf565fb2231410f5396f1ccd874191f43d5c6360
Hayato Kajiyama <me@piginbeer.com>
2014-10-07 13:49:52 +0900

    emacsのrainbow-delimitersの警告がうざいので対策

--- inits/13-rainbow-delimiters.el
+++ inits/13-rainbow-delimiters.el
@@ -1,4 +1,4 @@
 (require 'rainbow-delimiters)
-(add-to-list 'rainbow-delimiters-ignore-modes 'fundamental-mode)    
 (custom-set-faces '(rainbow-delimiters-depth-2-face ((t (:foreground "#E0FFB0")))))
-(global-rainbow-delimiters-mode 1)
+
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
