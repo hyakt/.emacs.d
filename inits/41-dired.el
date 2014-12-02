6aed543c2e4e51ac84001fc7cd1005812df3396c
Hayato Kajiyama <me@piginbeer.com>
2014-12-02 16:54:07 +0900

    emacsのdired.elの設定を変更

--- inits/41-dired.el
+++ inits/41-dired.el
@@ -1,2 +1,5 @@
 (require 'dired)
 (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
+
+(define-key dired-mode-map (kbd "(") 'dired-hide-details-mode)
+(define-key dired-mode-map (kbd ")") 'dired-hide-details-mode)
