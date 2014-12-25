8282a43a63e97724e7f07b05ada7e1c536028110
Hayato Kajiyama <me@piginbeer.com>
2014-12-25 20:10:24 +0900

    emacsにflycheck-pos-tipの設定を追加

--- inits/13-flycheck.el
+++ inits/13-flycheck.el
@@ -1,3 +1,8 @@
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (setq flycheck-highlighting-mode 'lines)
+
+;; flycheck-pos-tip
+(eval-after-load 'flycheck
+  '(custom-set-variables
+   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
