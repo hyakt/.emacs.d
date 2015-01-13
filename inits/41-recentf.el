572f8ebe85957a017dd380a5366e78c9d05018cc
Hayato Kajiyama <me@piginbeer.com>
2015-01-14 03:29:48 +0900

    41-recentfの設定を変更

--- inits/41-recentf.el
+++ inits/41-recentf.el
@@ -1,8 +1,9 @@
-(when (require 'recentf nil t)
+(use-package recentf-ext
+  :config
   (setq recentf-max-saved-items 2000)
   (setq recentf-exclude '(".recentf"))
   (setq recentf-auto-cleanup 10)
   (setq recentf-auto-save-timer
-        (run-with-idle-timer 30 t 'recentf-save-list))
+        (run-with-idle-timer 60 t 'recentf-save-list))
   (recentf-mode 1)
-  (require 'recentf-ext))
+  )
