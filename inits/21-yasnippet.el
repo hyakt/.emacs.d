10505324f457e56fe546c4d992e7bfa59398483d
Hayato Kajiyama <kaji1216@gmail.com>
2015-01-14 01:52:41 +0900

    21-yasnippetの設定を変更

--- inits/21-yasnippet.el
+++ inits/21-yasnippet.el
@@ -1,7 +1,8 @@
-(require 'yasnippet)
+(use-package yasnippet
+  :config
+  (setq yas-snippet-dirs
+        '("~/.emacs.d/yasnippet-snippets"))
 
-(setq yas-snippet-dirs
-      '("~/.emacs.d/yasnippet-snippets"))
-
-;; yas起動
-(yas-global-mode 1)
+  ;; yas起動
+  (yas-global-mode 1)
+  )
