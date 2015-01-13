a6ac05b44144a79d7510bcb938369983efada5b9
Hayato Kajiyama <kaji1216@gmail.com>
2015-01-14 03:21:04 +0900

    41-bmの設定を変更

--- inits/41-bm.el
+++ inits/41-bm.el
@@ -1,12 +1,15 @@
-(require 'bm)
-(add-hook 'find-file-hook 'bm-buffer-restore)
-(add-hook 'kill-buffer-hook 'bm-buffer-save)
-(add-hook 'after-save-hook 'bm-buffer-save)
-(add-hook 'after-revert-hook 'bm-buffer-restore)
-(add-hook 'vc-before-checkin-hook 'bm-buffer-save)
-(add-hook 'kill-emacs-hook '(lambda nil
-                              (bm-buffer-save-all)
-                              (bm-repository-save)))
-(global-set-key (kbd "M-@") 'bm-toggle)
-(global-set-key (kbd "M-[") 'bm-previous)
-(global-set-key (kbd "M-]") 'bm-next)
+(use-package bm
+  :bind(((kbd "M-@") . bm-toggle)
+        ((kbd "M-[") . bm-previous)
+        ((kbd "M-]") . bm-next))
+
+  :config
+  (add-hook 'find-file-hook 'bm-buffer-restore)
+  (add-hook 'kill-buffer-hook 'bm-buffer-save)
+  (add-hook 'after-save-hook 'bm-buffer-save)
+  (add-hook 'after-revert-hook 'bm-buffer-restore)
+  (add-hook 'vc-before-checkin-hook 'bm-buffer-save)
+  (add-hook 'kill-emacs-hook '(lambda nil
+                                (bm-buffer-save-all)
+                                (bm-repository-save)))
+)
