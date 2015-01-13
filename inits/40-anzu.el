4f115f2c09c5cf094b1ae91767ad68e3b759e7ed
Hayato Kajiyama <me@piginbeer.com>
2015-01-14 02:35:27 +0900

    40-anzuの設定を変更

--- inits/40-anzu.el
+++ inits/40-anzu.el
@@ -1,13 +1,14 @@
-(require 'anzu)
+(use-package anzu
+  :config
+  (global-anzu-mode t)
+  (setq anzu-use-migemo t)
+  (setq anzu-search-threshold 1000)
+  (setq anzu-minimum-input-length 3)
 
-(global-anzu-mode t)
-(setq anzu-use-migemo t)
-(setq anzu-search-threshold 1000)
-(setq anzu-minimum-input-length 3)
+  (set-face-attribute 'anzu-mode-line nil
+                      :foreground "yellow" :weight 'bold)
 
-(set-face-attribute 'anzu-mode-line nil
-                    :foreground "yellow" :weight 'bold)
-
-(global-set-key (kbd "C-%") 'anzu-query-replace)
-(global-set-key (kbd "M-%") 'anzu-query-replace-regexp)
-(global-set-key (kbd "C-M-%") 'anzu-query-replace-at-cursor)
+  (global-set-key (kbd "C-%") 'anzu-query-replace)
+  (global-set-key (kbd "M-%") 'anzu-query-replace-regexp)
+  (global-set-key (kbd "C-M-%") 'anzu-query-replace-at-cursor)
+  )
