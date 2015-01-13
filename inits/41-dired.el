9da3b8a66d811f310fb996f85e80882d05dc39f4
Hayato Kajiyama <kaji1216@gmail.com>
2015-01-14 03:21:24 +0900

    41-diredの設定を変更

--- inits/41-dired.el
+++ inits/41-dired.el
@@ -1,5 +1,7 @@
-(require 'dired)
-(define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
+(use-package wdired
+  :config
+  (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
+  )
 
 (define-key dired-mode-map (kbd "(") 'dired-hide-details-mode)
 (define-key dired-mode-map (kbd ")") 'dired-hide-details-mode)
