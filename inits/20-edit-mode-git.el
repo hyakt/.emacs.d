2791bd06a9b08279ead4d934d0ec8f03da516770
Hayato Kajiyama <kaji1216@gmail.com>
2015-01-14 01:27:24 +0900

    20-edit-mode-gitの設定を変更

--- inits/20-edit-mode-git.el
+++ inits/20-edit-mode-git.el
@@ -1,20 +1,21 @@
-(require 'magit)
-(global-set-key (kbd "C-c g") 'magit-status)
+(use-package magit
+  :config
+  (global-set-key (kbd "C-c g") 'magit-status)
 
-(set-face-background 'magit-item-highlight "#202020")
-(set-face-foreground 'magit-diff-add "green")
-(set-face-foreground 'magit-diff-del "red")
-(set-face-foreground 'magit-diff-file-header "blue")
+  (set-face-background 'magit-item-highlight "#202020")
+  (set-face-foreground 'magit-diff-add "green")
+  (set-face-foreground 'magit-diff-del "red")
+  (set-face-foreground 'magit-diff-file-header "blue")
 
-(defun my/magit-quit-session ()
-  (interactive)
-  (kill-buffer)
-  (delete-window))
+  (defun my/magit-quit-session ()
+    (interactive)
+    (kill-buffer)
+    (delete-window))
 
-(define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+  (define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+  )
 
-(require 'git-gutter)
-(global-git-gutter-mode +1)
-
-(require 'gitconfig-mode)
-(require 'gitignore-mode)
+(use-package git-gutter
+  :config
+  (global-git-gutter-mode +1)
+  )
