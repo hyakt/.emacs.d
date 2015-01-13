bd398d8d0741e9f6ee8250f5ea7f46b735ee2ec8
Hayato Kajiyama <me@piginbeer.com>
2015-01-14 03:44:18 +0900

    60-google.elの設定を変更

--- inits/60-google.el
+++ inits/60-google.el
@@ -1,12 +1,15 @@
-(require 'google-translate)
-(require 'google-this)
+(use-package google-translate
+  :config
+  (custom-set-variables
+   '(google-translate-default-source-language "en")
+   '(google-translate-default-target-language "ja"))
+  )
 
-(custom-set-variables
-  '(google-translate-default-source-language "en")
-  '(google-translate-default-target-language "ja"))
-
-(setq google-this-location-suffix "co.jp")
-(defun google-this-url () "URL for google searches."
+(use-package google-this
+  :config
+  (setq google-this-location-suffix "co.jp")
+  (defun google-this-url () "URL for google searches."
   ;; 100件/日本語ページ/5年以内ならこのように設定する
-  (concat google-this-base-url google-this-location-suffix
-          "/search?q=%s&hl=ja&num=100&as_qdr=y5&lr=lang_ja"))
+         (concat google-this-base-url google-this-location-suffix
+                 "/search?q=%s&hl=ja&num=100&as_qdr=y5&lr=lang_ja"))
+  )
