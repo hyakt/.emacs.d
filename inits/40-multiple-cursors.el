6f65adc547e2cdb8befef64e5f016529cb867986
Hayato Kajiyama <me@piginbeer.com>
2015-01-14 02:55:25 +0900

    40-multiple-cursorsの設定を変更

--- inits/40-multiple-cursors.el
+++ inits/40-multiple-cursors.el
@@ -1,9 +1,6 @@
-(require 'multiple-cursors)
-(global-set-key (kbd "<C-M-return>") 'mc/edit-lines)
-(global-set-key (kbd "C-M-r") 'mc/mark-all-in-region)
-
-;; リージョンと一致する箇所で現在行より下にあるもの1つを追加
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-;; リージョンと一致する箇所で現在行より上にあるもの1つを追加
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-
+(use-package multiple-cursors
+  :bind
+  (("C->" . mc/mark-next-like-this)
+   ("C-<" . mc/mark-previous-like-this)
+   ("<C-M-return>" . mc/mark-all-dwim))
+  )
