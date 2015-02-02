8cac1944aaa19d7373c22bbaae4b9c04dfd931d9
Hayato Kajiyama <me@piginbeer.com>
2015-02-02 22:37:47 +0900

    emacsのyasnippetにpathを追加

--- inits/21-yasnippet.el
+++ inits/21-yasnippet.el
@@ -1,7 +1,8 @@
 (use-package yasnippet
   :config
   (setq yas-snippet-dirs
-        '("~/.emacs.d/yasnippet-snippets"))
+        '("~/.emacs.d/yasnippet-snippets"
+          "~/.emacs.d/my-snippets"))
 
   ;; yas起動
   (yas-global-mode 1)
