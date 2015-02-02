65dde2dcc7aaafdb3e53fdbf16b687e936882896
Hayato Kajiyama <me@piginbeer.com>
2015-02-02 22:47:44 +0900

    .emacs.dにmy-snippetsフォルダを作成

--- inits/21-yasnippet.el
+++ inits/21-yasnippet.el
@@ -1,8 +1,8 @@
 (use-package yasnippet
   :config
   (setq yas-snippet-dirs
-        '("~/.emacs.d/yasnippet-snippets"
-          "~/.emacs.d/my-snippets"))
+        '("~/.emacs.d/my-snippets"
+          "~/.emacs.d/yasnippet-snippets"))
 
   ;; yas起動
   (yas-global-mode 1)
