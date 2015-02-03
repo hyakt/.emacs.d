331c7055ea0029ae02d21bf22fd1d74fe44bd261
Hayato Kajiyama <kaji1216@gmail.com>
2015-02-03 14:46:49 +0900

    emacsのgoogle.elを修正

--- inits/60-google.el
+++ inits/60-google.el
@@ -5,11 +5,4 @@
    '(google-translate-default-target-language "ja"))
   )
 
-(use-package google-this
-  :config
-  (setq google-this-location-suffix "co.jp")
-  (defun google-this-url () "URL for google searches."
-  ;; 100件/日本語ページ/5年以内ならこのように設定する
-         (concat google-this-base-url google-this-location-suffix
-                 "/search?q=%s&hl=ja&num=100&as_qdr=y5&lr=lang_ja"))
-  )
+(use-package google-this)
