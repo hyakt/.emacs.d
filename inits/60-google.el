55fb1d091f4a1edf45960f8d884742bbea3f94a0
Hayato Kajiyama <me@piginbeer.com>
2015-05-11 17:36:35 +0900

    emacsのgoogle.elのキーバインドを変更

--- inits/60-google.el
+++ inits/60-google.el
@@ -1,5 +1,6 @@
 (use-package google-translate
-  :bind (("C-x t" . google-translate-at-point))
+  :bind (("C-x l" . google-translate-at-point)
+         ("C-x L" . google-translate-at-point-reverse))
   :init
   (custom-set-variables
   '(google-translate-default-source-language "en")
