4b873e20d75f1b8a44ad58d5f59fd6b6cc11ae93
Hayato Kajiyama <kaji1216@gmail.com>
2015-05-16 02:22:23 +0900

    emacsのwhitespace-styleからemptyを削除

--- inits/01-display.el
+++ inits/01-display.el
@@ -92,7 +92,6 @@
                                       trailing       ; 行末
                                       tabs           ; タブ
                                       spaces         ; スペース
-                                      empty          ; 先頭/末尾の空行
                                       space-mark     ; 表示のマッピング
                                       tab-mark
                                       ))
