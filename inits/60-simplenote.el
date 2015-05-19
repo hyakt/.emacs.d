a81d94baeea7b9a01bc46adfbe1e558d7c711e32
Hayato Kajiyama <me@piginbeer.com>
2015-05-19 17:25:40 +0900

    emacsにsimplenoteの設定を追加

---
+++ inits/60-simplenote.el
@@ -0,0 +1,10 @@
+(use-package simplenote2
+  :config
+  (require 'netrc)
+  (let* ((credentials (netrc-credentials "app.simplenote.com"))
+         (login (nth 0 credentials))
+         (password (nth 1 credentials)))
+    (setq simplenote-email login)
+    (setq simplenote-password password))
+  (simplenote2-setup)
+  (setq simplenote2-notes-mode 'org-mode))
