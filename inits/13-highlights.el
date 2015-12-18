97f694ece8d7af36a328152bc042053b93ccd19f
Hayato Kajiyama <me@piginbeer.com>
2015-12-18 18:52:48 +0900

    rainbow-identifiersを追加
    ハイライト系をまとめた

---
+++ inits/13-highlights.el
@@ -0,0 +1,15 @@
+
+(use-package smartparens-config
+  :config
+  (smartparens-global-mode t))
+
+;; シンボルの色付け
+(use-package rainbow-identifers
+  :init
+  (add-hook 'prog-mode-hook 'rainbow-identifiers-mode))
+
+;; 括弧の色付け
+(use-package rainbow-delimiters
+  :config
+  (custom-set-faces '(rainbow-delimiters-depth-2-face ((t (:foreground "#E0FFB0")))))
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
