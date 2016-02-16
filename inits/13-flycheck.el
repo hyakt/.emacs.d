a8ed87474f2d4f90bee8eb7b80c3478680e73dcf
Hayato Kajiyama <me@piginbeer.com>
2016-02-17 01:29:05 +0900

    flycheckの設定を修正

--- inits/13-flycheck.el
+++ inits/13-flycheck.el
@@ -1,8 +1,4 @@
-; flycheckの設定
-
 (use-package flycheck
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode)
-  (setq flycheck-highlighting-mode 'lines)
-  (custom-set-variables '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
-)
+  (flycheck-pos-tip-mode))
