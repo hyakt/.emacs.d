a2df045ae5e54b6095b30dbd92e95c17566e5105
Hayato Kajiyama <kaji1216@gmail.com>
2016-08-09 09:30:04 +0900

    marmaladeの設定を除去 Caskがうまくはしらないので

--- init.el
+++ init.el
@@ -18,7 +18,6 @@
 (require 'package)
 (setq package-user-dir "~/.emacs.d/elpa/")
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 ;;init-loaderの設定
