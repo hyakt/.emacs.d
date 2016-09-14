6f30fb3b75d39fc18ea6889e982d1bbbf41fbfb3
Hayato Kajiyama <kaji1216@gmail.com>
2016-09-15 01:21:24 +0900

    helmの設定を編集

--- inits/30-helm.el
+++ inits/30-helm.el
@@ -1,4 +1,5 @@
 (use-package helm-config
+  :init (helm-autoresize-mode 1)
   :bind
   (("C-x C-f" . helm-find-files)
    ("C-x C-r" . helm-recentf)
@@ -10,47 +11,10 @@
    ("M-x"     . helm-M-x)
    ("C-o" . helm-swoop)
    ("C-z b" . helm-elscreen))
-
   :config
   (helm-mode 1)
-
-  (define-key helm-map (kbd "C-h") 'delete-backward-char)
-  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-
-  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
-
-  ;; Disable helm in some functions
-  (add-to-list 'helm-completing-read-handlers-alist '(find-alternate-file . nil))
-
-  ;; Emulate `kill-line' in helm minibuffer
-  (setq helm-delete-minibuffer-contents-from-point t)
-  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
-    "Emulate `kill-line' in helm minibuffer"
-    (kill-new (buffer-substring (point) (field-end))))
-
-  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
-    "Execute command only if CANDIDATE exists"
-    (when (file-exists-p candidate)
-      ad-do-it))
-
-  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
-    "Transform the pattern to reflect my intention"
-    (let* ((pattern (ad-get-arg 0))
-           (input-pattern (file-name-nondirectory pattern))
-           (dirname (file-name-directory pattern)))
-      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
-      (setq ad-return-value
-            (concat dirname
-                    (if (string-match "^\\^" input-pattern)
-                        ;; '^' is a pattern for basename
-                        ;; and not required because the directory name is prepended
-                        (substring input-pattern 1)
-                      (concat ".*" input-pattern))))))
-
-  ;; helm-descbindsの設定
-  (require 'helm-descbinds)
-  (helm-descbinds-mode 1)
-
-  ;; helm-migemoをon
   (helm-migemo-mode +1))
+
+(use-package helm-descbinds
+  :config
+  (helm-descbinds-mode 1))
