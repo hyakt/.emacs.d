32132218ee39102e9f9c2c440826de6479e4024a
Hayato Kajiyama <hayato.kajiyama01@g.softbank.co.jp>
2016-11-22 09:54:20 +0900

    jdeeでmaven-projectを開くと勝手にコンパイルされる設定をnilに設定

--- inits/20-edit-mode-java.el
+++ inits/20-edit-mode-java.el
@@ -3,4 +3,7 @@
           '(lambda ()
              (define-key jdee-mode-map "\C-j" 'jdee-complete-minibuf)))
   :config
-  (setq jdee-server-dir "~/.emacs.d/jdee-server/target/"))
+  (setq jdee-server-dir "~/.emacs.d/jdee-server/target/")
+  (setq jdee-maven-init-hook nil))
+
+
