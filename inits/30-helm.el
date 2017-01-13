5c058b6da7f70627773595666fe83ba1ecd45651
Hayato Kajiyama <me@piginbeer.com>
2017-01-13 11:34:27 +0900

    helmの設定をまとめた

--- inits/30-helm.el
+++ inits/30-helm.el
@@ -1,3 +1,7 @@
+;;; 30-helm.el --- helm周りの設定
+;;; Commentary:
+
+;;; Code:
 (use-package helm-config
   :bind
   (("C-x C-f" . helm-find-files)
@@ -22,3 +26,35 @@
   :config
   (custom-set-variables
    '(helm-ag-base-command "rg --vimgrep --no-heading")))
+
+(use-package helm-gtags
+  :bind
+  (("M-t" . helm-gtags-find-tag)
+   ("M-r" . helm-gtags-find-rtag)
+   ("M-s" . helm-gtags-find-symbol)
+   ("M-g M-p" . helm-gtags-parse-file)
+   ("C-c <" . helm-gtags-previous-history)
+   ("C-c >" . helm-gtags-next-history)
+   ("M-," . helm-gtags-pop-stack)
+   ("M-." . helm-gtags-dwim))
+  :config
+  (custom-set-variables
+   '(helm-gtags-path-style 'relative)
+   '(helm-gtags-ignore-case t)
+   '(helm-gtags-auto-update t)))
+
+(use-package helm-dash
+  :bind (("C-x C-d" . helm-dash))
+  :config
+  (setq helm-dash-browser-func 'eww)
+
+  ;; 適応させたいモードとdocsetを記入
+  (defun my-set-doctype-for-helm-dash ()
+    (setq-local helm-dash-docsets
+                (case major-mode
+                  (js2-mode '("JavaScript" "jQuery"))
+                  (web-mode '("CSS" "HTML"))
+                  (java-mode '("Java"))
+                  (python-mode '("Python")))))
+  (dolist (hook (list 'js2-mode-hook 'web-mode-hook 'java-mode-hook 'python-mode-hook))
+    (add-hook hook 'my-set-doctype-for-helm-dash)))
