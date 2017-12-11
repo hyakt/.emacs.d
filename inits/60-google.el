1c2cc3b69d9b25e0a7517ea7b91c7fe65b5c24d4
Hayato Kajiyama <hayato.kajiyama01@g.softbank.co.jp>
2017-12-11 12:05:44 +0900

    不要なパッケージを棚卸し

--- inits/60-google.el
+++
@@ -1,9 +0,0 @@
-(use-package google-translate
-  :bind (("C-x l" . google-translate-at-point)
-         ("C-x L" . google-translate-at-point-reverse))
-  :init
-  (custom-set-variables
-  '(google-translate-default-source-language "en")
-  '(google-translate-default-target-language "ja")))
-
-(use-package google-this)
