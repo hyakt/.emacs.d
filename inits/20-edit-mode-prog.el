ba33793d10c588f7ca9183aba2854bcec4c130d5
Hayato Kajiyama <me@piginbeer.com>
2018-03-03 13:29:05 +0900

    update: web setting

--- inits/20-edit-mode-prog.el
+++ inits/20-edit-mode-prog.el
@@ -19,20 +19,30 @@
          ("\\.djhtml\\'" . web-mode)
          ("\\.html?\\'" . web-mode))
   :config
+
   (setq web-mode-engines-alist
         '(("php"    . "\\.phtml\\'")
-          ("blade"  . "\\.blade\\."))))
+          ("blade"  . "\\.blade\\.")))
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-indent-style 4)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-css-colorization t)
+  (setq web-mode-enable-current-element-highlight t)
+  (setq web-mode-enable-current-column-highlight t))
 
 ;; javascript
 (use-package js2-mode
   :mode (("\.js$" . js2-mode))
   :config
-  (setq js2-strict-missing-semi-warning nil))
+  (setq js2-strict-missing-semi-warning nil)
+  (setq js2-basic-offset 2))
 
 (use-package web-beautify
   :defer t
   :init
-  (add-hook 'js2-mode-hook (lambda ()(add-hook 'before-save-hook 'web-beautify-js-buffer t t)))
+  ;; (add-hook 'js2-mode-hook (lambda ()(add-hook 'before-save-hook 'web-beautify-js-buffer t t)))
   (add-hook 'json-mode-hook (lambda ()(add-hook 'before-save-hook 'web-beautify-js-buffer t t)))
   (add-hook 'css-mode (lambda ()(add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
 
