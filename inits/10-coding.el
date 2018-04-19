8d75edabe2bddf4b1b85f79dcca4b1dd346ac490
Hayato Kajiyama <hayato.kajiyama01@g.softbank.co.jp>
2018-04-19 12:31:14 +0900

    update: flycheck face

--- inits/10-coding.el
+++ inits/10-coding.el
@@ -54,15 +54,8 @@
 
 (use-package flyspell
   :init
-  (mapc                                   ;; flyspell-mode
-   (lambda (hook)
-     (add-hook hook 'flyspell-prog-mode))
-   '(emacs-lisp-mode-hook
-     python-mode-hook))
-
   (add-hook 'LaTeX-mode-hook 'flyspell-mode)
   (add-hook 'org-mode-hook 'flyspell-mode)
-
   :config
   (setq-default ispell-program-name "aspell")
   (add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
