333c884a28a8d2a3c96b6a5ebeb402f95dc3c21a
Hayato Kajiyama <hayato.kajiyama01@g.softbank.co.jp>
2018-05-29 16:16:49 +0900

    add: change font buffer elisp

--- elisp/encode.el
+++ elisp/encode.el
@@ -1,7 +1,6 @@
 ;; https://gist.github.com/kosh04/568800#file-emacs-xyzzy-unicode-un-escape-region
-
 ;; ユニコードエスケープシーケンスを解除する
-(defun unicode-unescape-region (start end)
+(defun my/unicode-unescape-region (start end)
   "指定した範囲のUnicodeエスケープ文字(\\uXXXX)をデコードする."
   (interactive "*r")
   (save-restriction
@@ -13,7 +12,7 @@
                      nil t))))
 
 ;; ユニコードエスケープシーケンスをする
-(defun unicode-escape-region (&optional start end)
+(defun my/unicode-escape-region (&optional start end)
   "指定した範囲の文字をUnicodeエスケープする."
   (interactive "*r")
   (save-restriction
@@ -28,4 +27,29 @@
 (defun char-unicode (char) (encode-char char 'ucs))
 (defun unicode-char (code) (decode-char 'ucs code))
 
-(provide 'unicode-escape)
+;; http://hins11.jugem.jp/?eid=34
+(defun url-encode-string (str &optional sys)
+  (let ((sys (or sys 'utf-8)))
+    (url-hexify-string (encode-coding-string str sys))))
+
+(defun url-decode-string (str &optional sys)
+  (let ((sys (or sys 'utf-8)))
+    (decode-coding-string (url-unhex-string str) sys)))
+
+(defun my/url-decode-region (beg end)
+  (interactive "r")
+  (let ((pos beg)
+        (str (buffer-substring beg end)))
+    (goto-char beg)
+    (delete-region beg end)
+    (insert (url-decode-string str 'utf-8))))
+
+(defun my/url-encode-region (beg end)
+  (interactive "r")
+  (let ((pos beg)
+        (str (buffer-substring beg end)))
+    (goto-char beg)
+    (delete-region beg end)
+    (insert (url-encode-string str 'utf-8))))
+
+(provide 'encode)
