78a219b7b27e1b1a5759f0e612e6a2a91a84a5ba
Hayato Kajiyama <me@piginbeer.com>
2018-07-18 06:03:51 +0900

    fix: inits

--- inits/30-interface.el
+++ inits/30-interface.el
@@ -42,86 +42,15 @@
   (use-package avy-migemo-e.g.swiper)
   (use-package avy-migemo-e.g.counsel))
 
-(use-package company
-  :ensure t
-  :config
-  (define-key company-active-map (kbd "\C-n") 'company-select-next)
-  (define-key company-active-map (kbd "\C-p") 'company-select-previous)
-  (define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
-  (setq company-transformers '(company-sort-by-occurrence))
-  (setq company-idle-delay 0.1)
-  (setq company-minimum-prefix-length 2)
-  (setq company-selection-wrap-around t)
-  (defvar company-mode/enable-yas t
-    "Enable yasnippet for all backends.")
-  (defun company-mode/backend-with-yas (backend)
-    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-        backend
-      (append (if (consp backend) backend (list backend))
-              '(:with company-yasnippet))))
-  (add-to-list 'company-backends 'company-anaconda)
-  (add-to-list 'company-backends 'company-shell)
-  (global-company-mode 1))
-
-(use-package quickrun
-  :bind (("C-x q" . quickrun)
-         ("C-x a" . quickrun-with-arg)))
-
-(use-package dumb-jump
-  :bind (("M-." . dumb-jump-quick-look)
-         ("M-n" . dumb-jump-go)
-         ("M-p" . dumb-jump-back))
-  :config (dumb-jump-mode)
-  (setq dumb-jump-default-project "")
-  (setq dumb-jump-max-find-time 10)
-  (setq dumb-jump-selector 'ivy))
-
-(use-package anzu
-  :bind (("C-%" . anzu-query-replace)
-         ("M-%" . anzu-query-replace-regexp)
-         ("C-M-%" . anzu-query-replace-at-cursor))
-  :config
-  (global-anzu-mode t)
-  (setq anzu-use-migemo t)
-  (setq anzu-search-threshold 1000)
-  (setq anzu-minimum-input-length 3)
-  (set-face-attribute 'anzu-mode-line nil :foreground "yellow" :weight 'bold))
-
-(use-package multiple-cursors
-  :bind
-  (("C->" . mc/mark-next-like-this)
-   ("C-<" . mc/mark-previous-like-this)
-   ("<C-M-return>" . mc/mark-all-dwim)))
-
-(use-package undo-tree
-  :bind (("M-/" . undo-tree-redo)
-         ("C-x u" . undo-tree-visualize))
-  :config
-  (global-undo-tree-mode t))
-
-(use-package expand-region
-  :bind (("C-," . er/expand-region)
-         ("C-M-," . er/contract-region)))
-
-(use-package wdired
-  :config
-  (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
-  (define-key dired-mode-map (kbd "(") 'dired-hide-details-mode)
-  (define-key dired-mode-map (kbd ")") 'dired-hide-details-mode))
-
-(use-package tramp
-  :if (eq system-type 'darwin)
-  :config
-  ;; http://qiita.com/l3msh0/items/6b84082541cbbf7d00f8
-  (setenv "TMPDIR" "/tmp"))
-
 (use-package multi-term
   :bind (("M-\\" . multi-term-dedicated-toggle))
   :config
   (add-to-list 'term-unbind-key-list "C-f")
   (add-to-list 'term-unbind-key-list "C-v"))
 
-(use-package open-junk-file :bind (("M-`" . open-junk-file)))
+(use-package which-key
+  :config
+  (which-key-mode))
 
 (use-package docker
   :bind(( "C-x c" . docker))
