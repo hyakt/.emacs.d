9d0581f891f0bc347750341509a85fff617fb5f5
Hayato Kajiyama <me@piginbeer.com>
2018-10-31 14:22:52 +0900

    update: theme setting

--- inits/50-frame.el
+++ inits/50-frame.el
@@ -40,7 +40,7 @@
   (global-unset-key (kbd "C-z"))
   (setq perspeen-keymap-prefix (kbd "C-z"))
   (set-face-attribute 'perspeen-selected-face nil
-                      :foreground "#ff90d0" :background nil))
+                      :foreground "#b084eb" :background nil))
 
 (use-package popwin
   :commands (popwin-mode)
