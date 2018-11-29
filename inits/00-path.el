705d1ead462a132df11e0c8a5bee5e0b9037c28c
Hayato Kajiyama <kajiyamah61@COAMAC17000052.local>
2018-11-29 13:32:26 +0900

    fix: change package system

--- inits/00-path.el
+++
@@ -1,9 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;; exec-pathの設定 ;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; http://sakito.jp/emacs/emacsshell.html#path
-;; より下に記述した物が PATH の先頭に追加されます
-(use-package exec-path-from-shell
-  :if (eq system-type 'darwin)
-  :config
-  (exec-path-from-shell-initialize))
