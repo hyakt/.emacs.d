a1ac8212db9c5dd9ecae2b5b55bd47b907f3170d
Hayato Kajiyama <me@piginbeer.com>
2019-01-08 15:23:21 +0900

    update: regex tool

--- inits/30-edit.el
+++ inits/30-edit.el
@@ -2,16 +2,13 @@
 ;;; Commentary:
 
 ;;; Code:
-(use-package anzu
-  :bind (("C-%" . anzu-query-replace)
-         ("M-%" . anzu-query-replace-regexp)
-         ("C-M-%" . anzu-query-replace-at-cursor))
+(use-package visual-regexp
+  :bind (("M-%" . vr/query-replace)))
+
+(use-package visual-regexp-steroids
+  :after visual-regexp
   :config
-  (global-anzu-mode t)
-  (setq anzu-use-migemo t)
-  (setq anzu-search-threshold 1000)
-  (setq anzu-minimum-input-length 3)
-  (set-face-attribute 'anzu-mode-line nil :foreground "yellow" :weight 'bold))
+  (setq vr/engine 'pcre2el))
 
 (use-package multiple-cursors
   :bind
