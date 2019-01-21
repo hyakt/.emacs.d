2771cc13121f9e76b5f057392ca1a872d556b4af
Hayato KAJIYAMA <kajiyamah61@COAMAC17000052.local>
2019-01-21 21:25:53 +0900

    fix: reopen-with-sudo-tramp

--- site-lisp/my-functions.el
+++ site-lisp/my-functions.el
@@ -134,12 +134,13 @@ BEG and END (region to sort)."
   (sleep-for 1.5)
   (sql-connect db))
 
-(defun reopen-with-sudo ()
+(defun reopen-with-sudo-tramp ()
   "Reopen current buffer-file with sudo using tramp."
   (interactive)
   (let ((file-name (buffer-file-name)))
+    (string-match ":\\(.*\\):" file-name)
     (if file-name
-        (find-alternate-file (concat "/sudo::" file-name))
+        (find-alternate-file (replace-regexp-in-string ":.*:" (concat ":" (match-string 1 file-name) "|sudo:root" ":") file-name))
       (error "Cannot get a file name"))))
 
 (provide 'my-functions)
