4c8fde397a33e1eacc8822e56e90a352bc3249fc
Hayato Kajiyama <me@piginbeer.com>
2019-05-08 11:42:32 +0900

    update: org-mode

--- inits/21-mode-doc.el
+++ inits/21-mode-doc.el
@@ -8,8 +8,7 @@
 
 ;; Org
 (use-package org
-  :mode (("\\.org$" . org-mode)
-         ("\\.txt$" . org-mode))
+  :mode (("\\.txt$" . org-mode))
   :bind (("C-M-]" . org-cycle-list-bullet)
          :map org-mode-map ("C-," . nil))
   :custom
@@ -17,6 +16,34 @@
   (org-src-fontify-natively t)
   (org-log-done 'time)
   :config
+  (defun my-add-custom-id ()
+    "Add \"CUSTOM_ID\" to the current tree if not assigned yet."
+    (interactive)
+    (my-org-custom-id-get nil t))
+
+  (defun my-get-custom-id ()
+    "Return a part of UUID with an \"org\" prefix. e.g. \"org3ca6ef0c\"."
+    (let* ((id (org-id-new "")))
+      (when (org-uuidgen-p id)
+        (downcase (concat "org"  (substring (org-id-new "") 0 8))))))
+
+  (defun my-org-custom-id-get (&optional pom create)
+    "See https://writequit.org/articles/emacs-org-mode-generate-ids.html"
+    (interactive)
+    (org-with-point-at pom
+      (let ((id (org-entry-get nil "CUSTOM_ID")))
+        (cond
+         ((and id (stringp id) (string-match "\\S-" id))
+          id)
+         (create
+          (setq id (my-get-custom-id))
+          (unless id
+            (error "Invalid ID"))
+          (org-entry-put pom "CUSTOM_ID" id)
+          (message "--- CUSTOM_ID assigned: %s" id)
+          (org-id-add-location id (buffer-file-name (buffer-base-buffer)))
+          id)))))
+
   (use-package ob-sql-mode)
   (use-package ox-gfm :after ox)
   (use-package ox-qmd :after ox
