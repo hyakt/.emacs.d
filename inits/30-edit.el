5a409f3a1d0047997d8498d189f16838610a6171
hyakt <hyakt0@gmail.com>
2019-07-17 10:44:03 +0900

    add: docker-tramp

--- inits/30-edit.el
+++ inits/30-edit.el
@@ -52,6 +52,8 @@
   ;; http://qiita.com/l3msh0/items/6b84082541cbbf7d00f8
   (setenv "TMPDIR" "/tmp"))
 
+(use-package docker-tramp)
+
 (use-package open-junk-file
   :bind (("C-`" . open-junk-file))
   :custom
