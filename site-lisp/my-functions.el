babaa4cdb5e145b3ca6074be80ddbb821be52323
Hayato Kajiyama <me@piginbeer.com>
2020-10-29 19:08:30 +0900

    add: projectile test other window

--- site-lisp/my-functions.el
+++ site-lisp/my-functions.el
@@ -291,8 +291,7 @@ the folder if it doesn't exist."
         (progn (unless (file-exists-p test-dir)
                  (make-directory test-dir :create-parents))
                (find-file-other-window (concat test-dir test-file))
-               (save-buffer))))
-    ))
+               (save-buffer))))))
 
 (defun my/generate-slack-reminder (content)
   "Generate slack reminder with CONTENT and copy to clipboard."
