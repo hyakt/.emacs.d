66a085d03112c96661a3105a3d136f15041ec9cb
Hayato Kajiyama <me@piginbeer.com>
2022-12-21 10:18:42 +0900

    update: appearances

---
+++ my-packages.el
@@ -0,0 +1,19 @@
+;;; my-packages.el --- My packages settings. -*- coding: utf-8; lexical-binding: t -*-
+;; Author: hyakt <https://github.com/hyakt/.emacs.d>
+
+;;; Commentary:
+;; This is hyakt's init.el of Emacs.
+
+(defconst my/packages
+    '(
+      ;; basic
+      exec-path-from-shell
+      gcmh
+      ;; appearances
+      all-the-icons
+      beacon-mode
+      volatile-highlights
+      doom-themes
+      dashboard
+      doom-modeline
+      ))
