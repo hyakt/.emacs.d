de3a3e61e8155e09b4f662e0e5e7b94c525a8d98
Hayato Kajiyama <me@piginbeer.com>
2023-06-16 17:18:42 +0900

    fix: consult-fd

--- init.el
+++ init.el
@@ -984,29 +984,27 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
    :preview-key "C-.")
 
   ;; https://github.com/minad/consult/wiki#find-files-using-fd
-  (defvar consult--fd-command nil)
   (defun consult--fd-builder (input)
-    (unless consult--fd-command
-      (setq consult--fd-command
-            (if (eq 0 (call-process-shell-command "fdfind"))
-                "fdfind"
-              "fd")))
-    (pcase-let* ((`(,arg . ,opts) (consult--command-split input))
-                 (`(,re . ,hl) (funcall consult--regexp-compiler
-                                        arg 'extended t)))
-      (when re
-        (list :command (append
-                        (list consult--fd-command
-                              "--color=never" "--full-path"
-                              (consult--join-regexps re 'extended))
-                        opts)
-              :highlight hl))))
+    (let ((fd-command
+           (if (eq 0 (process-file-shell-command "fdfind"))
+               "fdfind"
+             "fd")))
+      (pcase-let* ((`(,arg . ,opts) (consult--command-split input))
+                   (`(,re . ,hl) (funcall consult--regexp-compiler
+                                          arg 'extended t)))
+        (when re
+          (cons (append
+                 (list fd-command
+                       "--color=never" "--full-path"
+                       (consult--join-regexps re 'extended))
+                 opts)
+                hl)))))
 
   (defun consult-fd (&optional dir initial)
     (interactive "P")
-    (let* ((prompt-dir (consult--directory-prompt "Fd" dir))
-           (default-directory (cdr prompt-dir)))
-      (find-file (consult--find (car prompt-dir) #'consult--fd-builder initial)))))
+    (pcase-let* ((`(,prompt ,paths ,dir) (consult--directory-prompt "Fd" dir))
+                 (default-directory dir))
+      (find-file (consult--find prompt #'consult--fd-builder initial)))))
 
 (use-package consult-ghq
   :ensure t
