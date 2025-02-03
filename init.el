2373c74191126a2ec2674be22e67253ad415352b
Hayato Kajiyama <me@piginbeer.com>
2025-02-03 10:26:39 +0900

    fix: desktop ignore

--- init.el
+++ init.el
@@ -905,6 +905,11 @@
   :config
   (setq desktop-load-locked-desktop t)
   (setq desktop-save t)
+  (setq desktop-buffers-not-to-save
+        (concat "\\("
+                "^nn\\.a[0-9]+\\|\\.log\\|(ftp)\\|^tags\\|^TAGS"
+                "\\|\\.emacs.*\\|\\.diary\\|\\.newsrc-dribble\\|\\.gz\\|\\.bbdb"
+                "\\)$"))
   (add-to-list 'desktop-modes-not-to-save 'dired-mode)
   (add-to-list 'desktop-modes-not-to-save 'Info-mode)
   (add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
